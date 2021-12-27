e1cc71feda8b5d4daaa2c57a38b16a4e362af68a
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-27 15:24:20 +0900

    Done

--- order-summary/src/App.svelte
+++ order-summary/src/App.svelte
@@ -1,37 +1,41 @@
-<script>
-  import Card from "./components/Card.svelte";
+<script lang="ts">
+    import Card from "./components/Card.svelte";
 </script>
 
 <svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&display=swap"
-    rel="stylesheet"
-  />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&display=swap"
+        rel="stylesheet"
+    />
 </svelte:head>
 
 <div id="main">
-  <Card />
+    <Card />
 </div>
 
 <style>
-  #main {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: url("/assets/pattern-background-desktop.svg") rgb(220, 229, 253);
-    background-repeat: no-repeat;
-    background-size: contain;
-  }
-  @media screen and (max-width: 1440px) {
     #main {
-      background: url("/assets/pattern-background-mobile.svg")
-        rgb(220, 229, 253);
-      background-repeat: no-repeat;
-      background-size: contain;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: url("/assets/pattern-background-desktop.svg")
+            rgb(220, 229, 253);
+        background-repeat: no-repeat;
+        background-size: contain;
     }
-  }
+    @media screen and (max-width: 1440px) {
+        #main {
+            background: url("/assets/pattern-background-mobile.svg")
+                rgb(220, 229, 253);
+            background-repeat: no-repeat;
+            background-size: cover;
+            position: static;
+            padding: 50px 0;
+            height: auto;
+        }
+    }
 </style>
--- order-summary/src/components/Card.svelte
+++ order-summary/src/components/Card.svelte
@@ -1,27 +1,35 @@
 <script>
-  import Hero from "./Hero.svelte";
-  import Title from "./Title.svelte";
-  import Plan from "./Plan.svelte";
-  import Buttons from "./Buttons.svelte";
+    import Hero from "./Hero.svelte";
+    import Title from "./Title.svelte";
+    import Plan from "./Plan.svelte";
+    import Buttons from "./Buttons.svelte";
 </script>
 
 <div id="card">
-  <Hero />
-  <Title />
-  <Plan />
-  <Buttons />
+    <Hero />
+    <Title />
+    <Plan />
+    <Buttons />
 </div>
 
 <style>
-  #card {
-    width: 400px;
-    border-radius: 15px;
-    box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.25);
-    background-color: white;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    overflow: hidden;
-  }
+    #card {
+        width: 400px;
+        border-radius: 15px;
+        box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.25);
+        background-color: white;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        overflow: hidden;
+    }
+    @media all and (max-width: 1440px) {
+        #card {
+            position: static;
+            transform: none;
+            width: 90%;
+            margin: auto;
+        }
+    }
 </style>
